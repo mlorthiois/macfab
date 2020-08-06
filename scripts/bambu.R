8b13fa1d279a4b96694954a53ee355cebbc530d2
Fabien Sassolas <fabien.sassolas@etu.univ-lyon1.fr>
2020-08-06 15:15:50 +0200

    corrected issue caused by a bambu update

--- scripts/bambu.R
+++ scripts/bambu.R
@@ -1,11 +1,11 @@
-library(bambu)
-library(BSgenome)
+library("bambu")
+library("BSgenome")
 
 gtf.file <- snakemake@input[["gtf"]]
 test.bam <- snakemake@input[["bam"]]
 fa.file <- snakemake@input[["fa"]]
 
-bambuAnnotations <- prepareAnnotationsFromGTF(gtf.file)
+bambuAnnotations <- prepareAnnotations(gtf.file)
 
 se <- bambu(reads = test.bam, annotations = bambuAnnotations, genomeSequence = fa.file)
 writeBambuOutput(se, path = snakemake@output[["o_dir"]])
