fbf19aa9053b4b4dd93b0cc4dec4f04aad646234
Fabien Sassolas <fabien.sassolas@etu.univ-lyon1.fr>
2020-08-07 09:41:14 +0200

    fixed issues with bambu implementation

--- scripts/bambu.R
+++ scripts/bambu.R
@@ -9,4 +9,4 @@ bambuAnnotations <- prepareAnnotationsFromGTF(gtf.file)
 
 se <- bambu(reads = test.bam, annotations = bambuAnnotations, genomeSequence = fa.file)
 writeBambuOutput(se, path = snakemake@output[["o_dir"]])
-file.rename(paste(snakemake@input[[0]], "transcript_exon.gtf", sep=""), paste(snakemake@input[[0]], snakemake@output[["o_name"]], sep=""))
+file.rename(paste(snakemake@input[[1]], "transcript_exon.gtf", sep=""), paste(snakemake@input[[1]], snakemake@output[["o_name"]], sep=""))
