05903408b0bd3eba4428242cccc2bd9d90452077
Fabien Sassolas <fabien.sassolas@etu.univ-lyon1.fr>
2020-08-11 15:52:18 +0200

    increased flair ram

--- Snakefile.py
+++ Snakefile.py
@@ -114,7 +114,7 @@ rule flair:
     shadow: "shallow"
     threads:20
     resources:
-        ram="10G"
+        ram="30G"
     params:
         bed12=config["bed12ToGtf"], # needed to use the value in multi-lines shell command
         prefix="flair.{annot}" # software use prefix but prefix can't be used as output (because no file matching exactly this name will be created)
