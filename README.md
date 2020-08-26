0b14b1320d864b98a70812e6500ffd7fa6ae0a24
Fabien Sassolas <fabien.sassolas@etu.univ-lyon1.fr>
2020-08-26 11:28:52 +0200

    improved doc

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # MAC pipeline : Mapping, Analysis, Comparison
 # Installation
 ## Dependencies
-- snakemake (conda install)
+- snakemake (needs to be installed through conda)
 - python modules: intervaltree, kerneltree, tqdm, pybedtools, pysam v0.8.4+
-- Stringtie
-- Gffcompare
+- talon (**pip install** or locate softs in `~/.local/bin/`)
+- Stringtie : https://github.com/gpertea/stringtie
+- Gffcompare : http://ccb.jhu.edu/software/stringtie/gffcompare.shtml
 - paftools.js : latest version available on the minimap2 github (the conda one isn't working properly).
 - bed12ToGtf script
 ## Installation process
 Clone the git repo via git clone or use a release.
 # How to use
-First edit the `config.yaml` file to set paths to softs, datasets and the name of the cell line used.
+First edit the `config.yaml` file to set paths to softs, datasets and metadata.
 Then issue the command:
-`snakemake --use-conda -s Snakefile.py --cores [max threads] --cluster "sbatch --cpus-per-task={threads} --mem={resources.ram}" -j 5`
+`snakemake --use-conda -s Snakefile.py --cores [number] --cluster "sbatch --cpus-per-task={threads} --mem={resources.ram}" -j 5`
 - You can replace "Snakefile.py" with the path to the file but beware that the config file is expected to be in the current working dir.
 - The "cores" option set the max threads available to the rules.
-- Note that all rules are launched separately with their own RAM and CPU on the cluster : launching snakemake with the defaults values for the cluster is perfectly fine.
+- Note that all rules are launched separately with their own RAM and CPU on the cluster : launching snakemake with the defaults values for the cluster is fine.
+- full options description : https://snakemake.readthedocs.io/en/stable/executing/cli.html
--- Snakefile.py
+++ Snakefile.py
@@ -8,13 +8,17 @@ wildcard_constraints:
 configfile: "config.yaml" # path to the config file
 localrules: all, compact, configR # never launch the all and the compact rules on the cluster
 
-rule all: # a simple rule to launch the full pipeline without specifiying the final rule (graph)
+ # a simple rule to launch the full pipeline without specifiying the final rule (graph)
+rule all:
     input:
         results="Graph.recap.pdf"
     threads:1
     resources: # is used by snakemake as input for the sbatch command
         ram="6G"
 
+# a rule who installs a conda env with R, devtools et biocmanager
+# the script installs inside the R env bambu 0.2 (release URL) and BSgenome (needed by bambu)
+# ensure that the env is set before bambu, avoid conflicts when two bambu instances are launched at the same time
 rule configR:
     output: touch(".R_config")
     conda:
@@ -22,6 +26,8 @@ rule configR:
     script:
         "scripts/install.R"
 
+# a rule who creates one fastq file from the dir
+# bambu doesn't know how to use a dir
 rule compact:
     output:
        temp("compacted.fastq")
@@ -30,6 +36,8 @@ rule compact:
     shell:
         "cat {params} > {output}"
 
+# convert annotations for minimap
+# custom path because at the time of the writing the conda paftools.js doesn't work
 rule gtfToBed12:
     input:
         lambda wildcards: config["annotation"][wildcards.annot]
@@ -43,6 +51,7 @@ rule gtfToBed12:
     shell:
         config["paftools.js"] + " gff2bed {input} > {output}"
 
+# map fastq on the ref genome with annotations provided
 rule mapping:
     input:
         fastq=rules.compact.output,
@@ -57,7 +66,8 @@ rule mapping:
         ram="60G"
     shell:
         "minimap2 -t {threads} -ax splice --MD --junc-bed {input.bed} {input.fa} {input.fastq} > {output}"
-        
+
+# convert sam to bam and sort it      
 rule sam2bam:
     input:
         "minimap.{annot}.sam"
@@ -71,6 +81,9 @@ rule sam2bam:
     shell:
         "samtools view -b -@ {threads} {input} | samtools sort -o {output}"
 
+# analyze expression with bambu
+# asks for .R_config created by the R_config rule
+# a custom function is used to get the wildcards values ; see https://snakemake.readthedocs.io/en/stable/tutorial/advanced.html#step-3-input-functions
 rule bambu:
     input:
         gtf=lambda wildcards: config["annotation"][wildcards.annot],
@@ -79,7 +92,7 @@ rule bambu:
         isConfig=".R_config"
     output:
         o_name="bambu.{annot}.gtf"
-    shadow:
+    shadow: # snakemake symlinks the data, runs the soft inside the shadow dir then move the output out of it and delete it
         "shallow"
     conda:
         "envs/r.yaml"
@@ -88,7 +101,10 @@ rule bambu:
         ram="30G"
     script:
         "scripts/bambu.R"
-        
+
+# analyze expression using stringtie
+# stringtie needs to be installed and the path specified using the config file
+# no shadow dir here because stringtie only creates its output
 rule stringtie:
     input:
         gtf=lambda wildcards: config["annotation"][wildcards.annot],
@@ -101,6 +117,9 @@ rule stringtie:
     shell:
         config["stringtie"] + " -L -G {input.gtf} -o {output} -p {threads} {input.bam}"
 
+# annotates using flair
+# flair needs bed12 mapping
+# flair outputs bed who needs to be converted to gtf
 rule flair:
     input:
         gtf=lambda wildcards: config["annotation"][wildcards.annot],
@@ -125,7 +144,9 @@ rule flair:
         flair.py collapse -g {input.fa} -r {input.fastq} -q {params.prefix}_all_corrected.bed -o {params.prefix}
         {params.bed12} {params.prefix}.isoforms.bed > {output}
         """
-        
+# analyze expression using talon
+# creates a dedicated config file on the fly using information provided in the config file   
+# renames output to match the expected result  
 rule talon:
     input:
         fa=config["reference_path"],
@@ -144,17 +165,20 @@ rule talon:
     params:
         cell_line=config["cell_line"],
         prefix="talon.{annot}",
-        used_annot="{annot}"
+        used_annot="{annot}",
+        reference_build=config["reference_build"],
+        data_type=config["data_type"]
     shell:
         """
         ~/.local/bin/talon_label_reads --f {input.sam} --g {input.fa} --o {params.prefix} --t={threads}
-        ~/.local/bin/talon_initialize_database --f {input.gtf} --g CanFam3 --a {params.used_annot} --idprefix {params.prefix} --o {params.prefix}
-        echo {params.cell_line},Dog_transcript,nanopore,{params.prefix}_labeled.sam > talon.config
-        ~/.local/bin/talon --f talon.config --db {output.db} --build CanFam3 -t {threads} --o {threads}
-        ~/.local/bin/talon_create_GTF --db {output.db} -b CanFam3 -a {params.used_annot} --o {output.gtf}
+        ~/.local/bin/talon_initialize_database --f {input.gtf} --g {params.reference_build} --a {params.used_annot} --idprefix {params.prefix} --o {params.prefix}
+        echo {params.cell_line},{params.data_type},nanopore,{params.prefix}_labeled.sam > talon.config
+        ~/.local/bin/talon --f talon.config --db {output.db} --build {params.reference_build} -t {threads} --o {threads}
+        ~/.local/bin/talon_create_GTF --db {output.db} -b {params.reference_build} -a {params.used_annot} --o {output.gtf}
         mv {output.gtf}_talon.gtf {output.gtf}
         """
-        
+
+# intersect gtf with bam to keep only expressed genes and not those from the annotation        
 rule only_seen_exons:
     input:
         gtf="{software}.{annot}.gtf",
@@ -172,7 +196,8 @@ rule only_seen_exons:
         grep $'\t'exon$'\t' {input.gtf} > {output.exon}
         bedtools intersect -u -s -split -a {output.exon} -b {input.bam} > {output.final}
         """
-        
+
+# compare annotation to ref      
 rule gffcompare:
     input:
         test="{software}.{annot}.filtered.gtf",
@@ -191,7 +216,7 @@ rule gffcompare:
          {params.gffcompare} {input.test} -r {input.ref} -o {params.prefix}
          mv {params.prefix}.stats {output.result}
          """
-        
+# format values to use it as R input      
 rule parse_gffcompare:
     input:
         expand("{software}.{annot}.stats", software=SOFTWARE, annot=config["annotation"].keys())
@@ -203,7 +228,8 @@ rule parse_gffcompare:
         ram="6G"
     script:
         "scripts/gffparse.py"
- 
+
+# plot the expected results with ggplot2
 rule graph:
     input:
         Sensitivity="Sensitivity.gffparse.tsv",
